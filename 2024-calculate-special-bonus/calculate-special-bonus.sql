bafb07436c08ffb11ab37d596e168910093d0b06
Bhakti Vitthal Bhosale <124253684+bhosalebhakti309@users.noreply.github.com>
2026-03-03 22:12:55 +0530

    Time: 586 ms (50.93%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select  employee_id,
+case
+when employee_id % 2 = 0  or  substring(name, 1 ,1 ) ='M' 
+then salary = 0 
+else salary 
+end as bonus
+from employees
+order by employee_id;
